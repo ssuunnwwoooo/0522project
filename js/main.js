313e19b0035021ae5bf950f868d5c7017e3b4140
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-24 18:02:59 +0900

    제네시스 홈페이지

--- js/main.js
+++ js/main.js
@@ -6,4 +6,27 @@ $(function () {
         autoplaySpeed: 3000,
         pauseOnHover: false,
     });
+
+    $('.product_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+
+        ]
+    });
+
+
+    $('#product .arrows .left').on('click', function () {
+        $('.product_slide').slick('slickPrev');
+    });
+    $('#product .arrows .right').on('click', function () {
+        $('.product_slide').slick('slickNext');
+    });
 })
