9a727e611d8bcd67c11162f3a706164ea70c0453
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-25 18:09:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,15 +18,49 @@ $(function () {
                     slidesToShow: 2,
                 }
             },
-
         ]
     });
 
 
-    $('#product .arrows .left').on('click', function () {
-        $('.product_slide').slick('slickPrev');
+    $('.full_slide').slick({
+        //dots: true,
+        // dots: true,
+        // customPaging: function (slider, i) {
+        //     var thumb = $(slider.$slides[i]).data();
+        //     if (i == '0') {
+        //         i = "2023 CAR DESIGN AWARD";
+        //     } else if (i == '1') {
+        //         i = "2023 RED DOT DESIGN AWARD";
+        //     }
+        //     else if (i == '2') {
+        //         i = "2023 MOTORTREND CAR OF THE YEAR®";
+        //     }
+        //     else if (i == '3') {
+        //         i = "2022 RED DOT AWARDS";
+        //     }
+        //     else if (i == '4') {
+        //         i = "J.D. POWER 2022 U.S. INITIAL QUALITY STUDY";
+        //     }
+        //     else if (i == '5') {
+        //         i = "2022 IIHS Top Safety Pick+";
+        //     }
+        //     return '<a class="dot">' + i + '</a>';
+        // },
+
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
     });
-    $('#product .arrows .right').on('click', function () {
-        $('.product_slide').slick('slickNext');
-    });
+
+
+    $('.full_dots li').on('click', function () {
+        let idx = $(this).index();
+        $('.full_slide').slick('slickGoTo', idx);
+
+        $(this).addClass('on').siblings().removeClass('on');
+    })
+
+
+
 })
